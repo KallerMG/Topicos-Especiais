55805d4dc4ce4a47177403acb3d77b9cf8b37814
kallermg <kallermestre@gmail.com>
2021-04-05 21:02:03 -0300

    versão banco de dados

---
+++ banco.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="pcinfo"
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute("CREATE DATABASE pcinfo")
+
+
+#mycursor.execute("CREATE TABLE pcinfo (id INT AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255), cpu_uso VARCHAR(255), cpu_freq VARCHAR(255), cpu_media VARCHAR(255), cpu_temp VARCHAR(255), memo_uso VARCHAR(255), memo_disp VARCHAR(255), memo_porc VARCHAR(255), memo_livre VARCHAR(255), fan_vel VARCHAR(255) )")
+
+sql = "INSERT INTO pcinfo (data, cpu_uso, cpu_freq, cpu_media, cpu_temp, memo_uso, memo_disp, memo_porc, memo_livre, fan_vel) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+
+
